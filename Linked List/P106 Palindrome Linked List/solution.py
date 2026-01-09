2f0cea15a7844d45c8a36947daded76aa077e95e
achyu-dev <achyugeorge7744@gmail.com>
2026-01-09 18:26:31 +0530

    Added day 57 problems and solutions

---
+++ Linked List/P106 Palindrome Linked List/solution.py
@@ -0,0 +1,24 @@
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+        
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        nums = []
+
+        while head:
+            nums.append(head.val)
+            head = head.next
+        
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            if nums[l] != nums[r]: # when values are not equal
+                return False
+            l += 1
+            r -= 1
+        return True
