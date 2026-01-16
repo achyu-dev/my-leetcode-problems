765d050e16b428591cf3618fe44f827058ec880d
achyu-dev <achyugeorge7744@gmail.com>
2026-01-16 13:12:20 +0530

    Added day 64 solutions

--- Linked List/P118 Delete all occurrences of a key in DLL/solution.cpp
+++ Linked List/P118 Delete all occurrences of a key in DLL/solution.cpp
@@ -30,6 +30,50 @@ struct ListNode
 class Solution {
 public:
     ListNode * deleteAllOccurrences(ListNode* head, int target) {
-        
+        ListNode* temp = head;
+        while (temp != NULL) {
+            if (temp-> val == target) { // if current value is target
+                if (temp == head) {
+                    head = head->next;
+                }
+                ListNode* nextNode = temp->next;
+                ListNode* prevNode = temp->prev;
+
+                if (nextNode != NULL) {
+                    nextNode->prev = prevNode;
+                }
+                if (prevNode != NULL) {
+                    prevNode->next = nextNode;
+                }
+
+                delete temp;
+                temp = nextNode;
+            }
+            else {
+                temp = temp->next;
+            }
+        }
+        return head;
     }
 };
+
+int main() {
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->prev = head;
+    head->next->next = new ListNode(3);
+    head->next->next->prev = head->next;
+    head->next->next->next = new ListNode(2);
+    head->next->next->next->prev = head->next->next;
+    head->next->next->next->next = new ListNode(4);
+    head->next->next->next->next->prev = head->next->next->next;
+    int target = 2;
+    Solution sol;
+    head = sol.deleteAllOccurrences(head, target);
+    ListNode* temp = head;
+    while (temp != NULL) {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+    return 0;
+}
--- Linked List/P119 Find Pairs with Given Sum in Doubly Linked List/solution.cpp
+++ Linked List/P119 Find Pairs with Given Sum in Doubly Linked List/solution.cpp
@@ -27,11 +27,91 @@ struct ListNode
     }
 };
 
-
-class Solution {
+// TC - O(N^2), SC - O(1)
+class Solution
+{
 public:
-    vector<vector<int>> findPairsWithGivenSum(ListNode* head, int target) {
-        
+    vector<vector<int>> findPairsWithGivenSum(ListNode *head, int target)
+    {
+        vector<vector<int>> result;
+        ListNode *temp1 = head;
+        ListNode *temp2 = head;
 
+        while (temp1 != NULL)
+        {
+            temp2 = temp1->next;
+            while (temp2 != NULL && temp2->val + temp1->val <= target)
+            {
+                if (temp1->val + temp2->val == target)
+                {
+                    result.push_back({temp1->val, temp2->val});
+                }
+                temp2 = temp2->next;
+            }
+            temp1 = temp1->next; // Move to the next node
+        }
+        return result;
     }
 };
+
+
+// TC - O(N), SC - O(1)
+class Solution
+{
+public:
+    vector<vector<int>> findPairsWithGivenSum(ListNode *head, int target)
+    {
+        vector<vector<int>> result;
+        ListNode *left = head;
+        ListNode *right = head;
+
+        // Move right to the end of the list
+        while (right->next != NULL)
+        {
+            right = right->next;
+        }
+
+        while (left != NULL && right != NULL && left != right && right->next != left)
+        {
+            int currentSum = left->val + right->val;
+            if (currentSum == target)
+            {
+                result.push_back({left->val, right->val});
+                left = left->next;
+                right = right->prev;
+            }
+            else if (currentSum < target)
+            {
+                left = left->next;
+            }
+            else
+            {
+                right = right->prev;
+            }
+        }
+        return result;
+    }
+};
+
+int main()
+{
+    ListNode *head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->prev = head;
+    head->next->next = new ListNode(3);
+    head->next->next->prev = head->next;
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->prev = head->next->next;
+
+    Solution sol;
+    int target = 5;
+    vector<vector<int>> pairs = sol.findPairsWithGivenSum(head, target);
+
+    for (const auto &pair : pairs)
+    {
+        cout << "[" << pair[0] << ", " << pair[1] << "]" << endl;
+    }
+
+    // Clean up memory (not shown)
+    return 0;
+}
