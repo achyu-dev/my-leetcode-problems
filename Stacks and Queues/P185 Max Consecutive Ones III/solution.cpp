9540b47b40cf010620f3a3a5202d909318e77efc
achyu-dev <achyugeorge7744@gmail.com>
2026-03-17 09:43:18 +0530

    Added day 100 problems and solutions

---
+++ Stacks and Queues/P185 Max Consecutive Ones III/solution.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int left = 0, maxLength = 0, zeroCount = 0;
+
+        for (int right = 0; right < nums.size(); ++right) {
+            if (nums[right] == 0) {
+                zeroCount++;
+            }
+            while (zeroCount > k) {
+                if (nums[left] == 0) {
+                    zeroCount--;
+                }
+                left++;
+            }
+            maxLength = max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
+};
+
+
+int main() {
+    Solution solution;
+    vector<int> nums = {1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0};
+    int k = 2;
+    cout << solution.longestOnes(nums, k) << endl;
+    return 0;
+}
